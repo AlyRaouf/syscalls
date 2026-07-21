ba00ef61ff044452139807336766c378a0849a39
AlyRaouf <aly.raouf123@gmail.com>
2026-07-21 13:50:15 +0300

    Replace return with exit for error handling

--- mv.c
+++ mv.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) return 1;
+    if (argc != 3) exit(-1);
 
     if (rename(argv[1], argv[2]) != 0) {
-        return 1;
+        exit(-2);
     }
 
     return 0;
