3507f6132b6f9fee34092d011b963fb7b2ca5f59
AlyRaouf <aly.raouf123@gmail.com>
2026-07-23 14:45:18 +0300

    Update echo.c

--- echo.c
+++ echo.c
@@ -4,11 +4,9 @@
 int main(int argc, char *argv[])
 {
 
-    for (int i = 1; i < argc; i++) {
-        printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
-    }
+    for (int i = 1; i < argc; i++)
+    printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
     printf("\n");
     return 0;
-
 
 }
